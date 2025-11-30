76d66e174768eb3df53464ff0957b05df7f1de7d
Abhisheky77 <123infinity890@gmail.com>
2025-11-30 15:46:33 +0530

    LoginPage

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Agar project me multiple page create karna ho to like (login open hoga ek new page me ,Registersion open hoga ek new page me ) to humko install kar na padega -----> react-router-dom
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+// import './App.css'
+// import { Button } from './components/ui/button'
+
+// function App() {
+
+//   return (
+//     <>
+//     <div className=' bg-amber-300'>
+//       <Button>Click</Button> 
+//     </div>
+//     </>
+//   )
+// }
+
+// export default App
